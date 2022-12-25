be439711767707d16cd1c8ee91fe824140eaab45
Daniel Molnar <molnard89@gmail.com>
2022-12-26 00:20:04 +0100

    day 12 task 2 done

--- day12/explore.py
+++ day12/explore.py
@@ -21,34 +21,56 @@ def check_adjacency(char1, char2):
     return ord(char2) - ord(char1) < 2
 
 
-def step_through(map_grid, N):
+def step_through(map_grid, starting_points, N):
     line_length, row_length = len(map_grid), len(map_grid[0].strip())
     distance_grid = np.zeros([line_length, row_length])
-    start_line, start_row = find_letter_in_grid(map_grid, "S")
+    start_line, start_row = starting_points
     distance_grid[start_line][start_row] = 1
 
     for k in range(1, N+1):
         for i in range(line_length):
             for j in range(row_length):
                 if distance_grid[i][j] == k:
-                    current_elevation = lines[i][j]
+                    current_elevation = map_grid[i][j]
                     if current_elevation == "E":
-                        print(f"Found the end point, its distance is {k-1}")
-                        return distance_grid
-                    if (i > 0) and (distance_grid[i-1][j]) == 0 and (check_adjacency(current_elevation, lines[i-1][j])):
+                        return k-1
+                    if (i > 0) and (distance_grid[i-1][j]) == 0 and (check_adjacency(current_elevation, map_grid[i-1][j])):
                         distance_grid[i-1][j] = k+1
-                    if (j > 0) and (distance_grid[i][j-1]) == 0 and (check_adjacency(current_elevation, lines[i][j-1])):
+                    if (j > 0) and (distance_grid[i][j-1]) == 0 and (check_adjacency(current_elevation, map_grid[i][j-1])):
                         distance_grid[i][j-1] = k+1
-                    if (i < (line_length-1)) and (distance_grid[i+1][j]) == 0 and (check_adjacency(current_elevation, lines[i+1][j])):
+                    if (i < (line_length-1)) and (distance_grid[i+1][j]) == 0 and (check_adjacency(current_elevation, map_grid[i+1][j])):
                         distance_grid[i+1][j] = k+1
-                    if (j < (row_length-1)) and (distance_grid[i][j+1]) == 0 and (check_adjacency(current_elevation, lines[i][j+1])):
+                    if (j < (row_length-1)) and (distance_grid[i][j+1]) == 0 and (check_adjacency(current_elevation, map_grid[i][j+1])):
                         distance_grid[i][j+1] = k+1
-    return distance_grid
+    return k-1
+
+
+def find_all_occurances(str_grid, elevation):
+    coordinates = []
+    for i, line in enumerate(str_grid):
+        for j, char in enumerate(line):
+            if char == elevation:
+                coordinates.append((i, j))
+    return coordinates
 
 
 if __name__ == "__main__":
     with open("map.txt") as f:
-        lines = f.readlines()
+        elevation_map = f.readlines()
+
+    a_levels = find_all_occurances(elevation_map, "a")
+    print(f"Number of positions with 'a' level = {len(a_levels)}")
 
     steps = 1000
-    dist_grid = np.array(step_through(lines, steps))
+    distance = step_through(elevation_map, find_letter_in_grid(elevation_map, "S"), steps)
+    print(f"Distance required to reach 'E' is {distance}")
+
+    shortest_distance = distance
+    print("Computing closest 'a' level to 'E'...")
+    for i, starting_coordinate in enumerate(a_levels):
+        distance = step_through(elevation_map, starting_coordinate, shortest_distance+1)
+        shortest_distance = min([distance, shortest_distance])
+        if i % 10 == 0:
+            print(f"{i/len(a_levels)*100} % done")
+
+    print(f"Closest 'a' elevation point to 'E' is {shortest_distance}")
